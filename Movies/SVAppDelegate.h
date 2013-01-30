bbe94cbb98bad57836b70366d4d08d5e9cf5a044
Sebastien Villar <sebastien.villar@gmail.com>
2013-01-30 18:20:50 +0200

    Initial Commit

---
+++ Movies/SVAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SVAppDelegate.h
+//  Movies
+//
+//  Created by Sébastien Villar on 30/01/13.
+//  Copyright (c) 2013 Sébastien Villar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SVAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Movies/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Movies
+//
+//  Created by Sébastien Villar on 30/01/13.
+//  Copyright (c) 2013 Sébastien Villar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SVAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([SVAppDelegate class]));
+	}
+}
